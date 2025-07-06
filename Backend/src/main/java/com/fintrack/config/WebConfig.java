fbeafd89ab52934abbed14cd218be99929e84218
Atharva <butteatharva2005@gmail.com>
2025-07-06 17:09:20 +0530

    Resolve cors problem

--- Backend/src/main/java/com/fintrack/config/WebConfig.java
+++ Backend/src/main/java/com/fintrack/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.fintrack.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,5 +13,15 @@ public class WebConfig implements WebMvcConfigurer {
         registry
                 .addResourceHandler("/uploads/**")
                 .addResourceLocations("file:uploads/"); // Adjust if uploads is in different folder
+    }
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://yourfrontend.com") // ✅ React domain
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true); // if you're using cookies
     }
 }
