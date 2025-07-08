199353027d9727dfaa554a8b2ea5d027d4ae6945
Atharva <butteatharva2005@gmail.com>
2025-07-08 16:35:33 +0530

    add resourse handler

--- Backend/src/main/java/com/fintrack/config/WebConfig.java
+++ Backend/src/main/java/com/fintrack/config/WebConfig.java
@@ -1,7 +1,6 @@
 package com.fintrack.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,12 +15,12 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://yourfrontend.com") // ✅ React domain
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true); // if you're using cookies
-    }
+    // @Override
+    // public void addCorsMappings(CorsRegistry registry) {
+    //     registry.addMapping("/**")
+    //             .allowedOrigins("https://yourfrontend.com") // ✅ React domain
+    //             .allowedMethods("*")
+    //             .allowedHeaders("*")
+    //             .allowCredentials(true); // if you're using cookies
+    // }
 }
